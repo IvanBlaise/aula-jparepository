7dba10c7a48f4d67788a86367d1664ee06c8c1a7
Ivan Carlos <ivancarlos50@gmail.com>
2022-12-01 10:15:36 -0300

    Fim aula jpaRepository

--- src/main/java/com/devsuperior/aulajparepository/repositories/UserRepository.java
+++ src/main/java/com/devsuperior/aulajparepository/repositories/UserRepository.java
@@ -13,5 +13,12 @@ public interface UserRepository extends JpaRepository<User, Long>{
 	
 	@Query("SELECT obj FROM User obj WHERE obj.salary >= :minSalary AND obj.salary <= :maxSalary")
 	Page<User> searchBySalary(Double minSalary, Double maxSalary, Pageable pageable);
+	
+	@Query("SELECT obj FROM User obj WHERE LOWER(obj.name) LIKE LOWER(CONCAT('%',:name,'%'))")
+	Page<User> searchByName(String name, Pageable pageable);
+	
+	Page<User> findBySalaryBetween(Double minSalary, Double maxSalary, Pageable pageable);
+	
+	Page<User> findByNameContainingIgnoreCase(String name, Pageable pageable);
 
 }
